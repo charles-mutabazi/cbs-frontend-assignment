fdef36637456ee8d94941610cdfed762336e70cd
Charles Mutabazi <charles.mutabazi@oneacrefund.org>
2024-08-27 21:23:43 +0200

    chore: Update Vite server configuration to listen on all interfaces

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  }, 
+  // set the server to listen on all interfaces
+  server: {
+    port: 8080,
+    host: '0.0.0.0'
   }
 })
